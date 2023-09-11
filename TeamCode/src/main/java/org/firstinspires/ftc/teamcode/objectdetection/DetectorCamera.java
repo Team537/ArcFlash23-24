196a63cb3601df8b0195aded49d8cc5a2d659dc9
Aniket Singh <singani@hamiltonstudents.org>
2023-09-10 20:56:04 -0500

    feat: detector camera states

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objectdetection/DetectorCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objectdetection/DetectorCamera.java
@@ -15,6 +15,7 @@ public class DetectorCamera {
     private ColorDetectionPipeline colorDetectionPipeline;
     private PropDetectionPipeline propDetectionPipeline;
     private List<ColorDetectionPipeline.ColorRegion> detectedRegions;
+    private CameraState currentState;
 
     public DetectorCamera(RobotHardware robot) {
         webcam = robot.webcam1;
@@ -49,23 +50,37 @@ public class DetectorCamera {
 
     public void setColorDetect() {
         webcam.setPipeline(colorDetectionPipeline);
+        currentState = CameraState.COLOR_DETECT;
+
     }
     public void setPropDetect(){
         webcam.setPipeline(propDetectionPipeline);
+        currentState = CameraState.PROP_DETECT;
     }
 
     public void loop() {
-        detectedRegions = colorDetectionPipeline.getColorRegions();
 
-        telemetry.addData("Detected Regions", detectedRegions.size());
-        for (ColorDetectionPipeline.ColorRegion region : detectedRegions) {
-            telemetry.addData("Region Center: %6.1f", region.center);
-            telemetry.addData("Region Width: %6.1f", region.boundingRect.width);
-            telemetry.addData("Region Height: %6.1f", region.boundingRect.height);
-            telemetry.addData("Region Area: %6.1f", region.boundingRect.area());
-            telemetry.addData("Region X: %6.1f", region.relativePosition.x);
-            telemetry.addData("Region Y: %6.1f", region.relativePosition.y);
+        if(currentState == CameraState.COLOR_DETECT) {
+            detectedRegions = colorDetectionPipeline.getColorRegions();
+
+            telemetry.addData("Detected Regions", detectedRegions.size());
+            for (ColorDetectionPipeline.ColorRegion region : detectedRegions) {
+                telemetry.addData("Region Center: %6.1f", region.center);
+                telemetry.addData("Region Width: %6.1f", region.boundingRect.width);
+                telemetry.addData("Region Height: %6.1f", region.boundingRect.height);
+                telemetry.addData("Region Area: %6.1f", region.boundingRect.area());
+                telemetry.addData("Region X: %6.1f", region.relativePosition.x);
+                telemetry.addData("Region Y: %6.1f", region.relativePosition.y);
+            }
         }
+        else if(currentState == CameraState.PROP_DETECT){
+            telemetry.addData("Prop Center X", propDetectionPipeline.getObjectCenter().x);
+            telemetry.addData("Prop Center Y", propDetectionPipeline.getObjectCenter().y);
+        }
+    }
 
+    public enum CameraState{
+        COLOR_DETECT,
+        PROP_DETECT
     }
 }
