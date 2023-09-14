00deec9d6e0215c3b6a8b8d2559f29ab45c79aff
Aniket Singh <singani@hamiltonstudents.org>
2023-09-13 19:37:45 -0500

    feat: More stuff logged

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testops/BatteryTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testops/BatteryTesting.java
@@ -7,10 +7,13 @@ import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.outoftheboxrobotics.photoncore.PhotonCore;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.Globals;
 import org.firstinspires.ftc.teamcode.Log;
 import org.firstinspires.ftc.teamcode.Pose;
@@ -31,10 +34,12 @@ public class BatteryTesting extends CommandOpMode {
     private ExpansionHubEx hub;
     private GamepadEx gamepadEx;
     private Log log;
+    private BNO055IMU imu;
+    private VoltageSensor voltageSensor;
     private static double MAX_X_SPEED = 5.0;
     private static double MAX_Y_SPEED = 5.0;
     private static double MAX_TURN_SPEED = 180;
-
+    LynxModule control_hub;
 
     @Override
     public void initialize() {
@@ -50,8 +55,9 @@ public class BatteryTesting extends CommandOpMode {
         motor2 = robot.frontRightMotor;
         motor3 = robot.backLeftMotor;
         motor4 = robot.backRightMotor;
-
-
+        imu = robot.imu;
+        control_hub = (LynxModule) hardwareMap.get(LynxModule.class, "Control Hub");
+        voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
         robot.enabled = true;
 
@@ -70,7 +76,17 @@ public class BatteryTesting extends CommandOpMode {
         motor3.setPower(gamepadEx.getLeftY());
         motor4.setPower(gamepadEx.getLeftY());
 
-    log.addData(hub.read12vMonitor(ExpansionHubEx.VoltageUnits.VOLTS));
+    log.addData(voltageSensor.getVoltage());
+    log.addData(control_hub.getCurrent(CurrentUnit.AMPS));
+    log.addData(control_hub.getGpioBusCurrent(CurrentUnit.AMPS));
+    log.addData(control_hub.getI2cBusCurrent(CurrentUnit.AMPS));
+    log.addData(imu.getPosition().x);
+    log.addData(imu.getPosition().y);
+    log.addData(imu.getPosition().z);
+    log.addData(motor1.getCurrent(CurrentUnit.AMPS));
+    log.addData(motor2.getCurrent(CurrentUnit.AMPS));
+    log.addData(motor3.getCurrent(CurrentUnit.AMPS));
+    log.addData(motor4.getCurrent(CurrentUnit.AMPS));
 
         telemetry.update();
 
